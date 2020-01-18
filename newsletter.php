a360e6310368859fb13ce16708cbec9f040f7b21
Juan Garcia <jgarcia1545@gmail.com>
2020-01-17 19:48:54 -0800

    Changed email

--- newsletter.php
+++ newsletter.php
@@ -2,7 +2,7 @@
 if(isset($_POST['news']))
 $news = $_POST['news'];
 
-$recipient = "harman2to1@gmail.com";
+$recipient = "EmiliosNewsletter@gmail.com";
 mail($recipient, "New Newsletter Subscriber", "Test Test") or die("Error!");
 echo "Email sent!";
 
