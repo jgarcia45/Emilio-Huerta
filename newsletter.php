3a28f31d12de25a98efd8cb886ff3c37fff2a227
Juan Garcia <jgarcia1545@gmail.com>
2020-01-17 19:46:53 -0800

    Put Meta Tag on each html page, changed email on php files

--- newsletter.php
+++ newsletter.php
@@ -2,7 +2,7 @@
 if(isset($_POST['news']))
 $news = $_POST['news'];
 
-$recipient = "harman2to1@gmail.com";
+$recipient = "EmiliosNewsletter@gmail.com";
 mail($recipient, "News Letter Subscriber", "Test Test") or die("Error!");
 echo "Email sent!";
 
