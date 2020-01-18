79b9788ebb50549715c65314307ad9de9893ad58
Juan Garcia <jgarcia1545@gmail.com>
2020-01-17 18:34:24 -0800

    Created PHP file for newsletter

---
+++ js/newsletter.js
@@ -0,0 +1,38 @@
+function newsletter() {
+    var email = document.getElementById('email').value;
+    document.getElementById('email').className = "form-control is-valid"
+    if (email == "") {
+        document.getElementById('email').placeholder = "Email cannot be empty";
+        document.getElementById('email').className = "form-control is-invalid"
+        return false;
+    } else {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if (!re.test(email)) {
+            window.alert("Email format invalid");
+            document.getElementById('email').className = "form-control is-invalid"
+            return false;
+        }
+    }
+
+    formData = {
+        'email': $('input[name=email]').val()
+    };
+
+
+    $.ajax({
+        url: "newsletter.php",
+        type: "POST",
+        data: formData,
+        success: function (data, textStatus, jqXHR) {
+
+            $('#status').text(data.message);
+            if (data.code) //If mail was sent successfully, reset the form.
+                $('#news').closest('form').find("input[type=text], textarea").val("");
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            $('#status').text(jqXHR);
+        }
+    });
+
+    document.getElementById('news').submit();
+}
